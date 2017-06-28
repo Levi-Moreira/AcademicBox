351bb8dbedb7199fff5f83bfeae38c9736d4f0b0
IFCE <IFCE@bepids-iMac-2.local>
2017-06-28 17:01:08 -0300

    fix join view controller bug

--- AcademicBox/ViewController.swift
+++ AcademicBox/ViewController.swift
@@ -51,6 +51,8 @@ class ViewController: UIViewController {
         handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
             
         })
+    
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
