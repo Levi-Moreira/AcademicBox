8c06616e5dea1400c85668f88b7ee124c14f834f
Alan Jeferson <alan.jeferson11@gmail.com>
2017-07-02 17:49:00 -0300

    UI Improvements on the Upload files controller

--- AcademicBox/UploadFilesViewController.swift
+++ AcademicBox/UploadFilesViewController.swift
@@ -9,21 +9,83 @@
 import UIKit
 import FirebaseDatabase
 
-class UploadFilesViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class UploadFilesViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    @IBOutlet weak var textFieldMaterialName: UITextField!
+    @IBOutlet weak var collectionViewImages: UICollectionView!
+
+    private let kCollectionViewCellUploadImages = "UploadFilesImagesCollectionViewCell"
     
     let reference = Database.database().reference(withPath: "materials")
     let user = AppUser.loggedUser
     let imagePicker = UIImagePickerController()
     
+    var images = [Material]()
+    
     var didUploadMaterial: ((Material) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupView()
         self.imagePicker.delegate = self
     }
+    
+    func setupView() {
+        self.collectionViewImages.dataSource = self
+        self.collectionViewImages.delegate = self
+    }
+    
+    
+    // MARK:- CollectionViewFlowLayout Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 90.0, height: 90.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10.0, left: 5.0, bottom: 10.0, right: 5.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row < self.images.count {
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCollectionViewCellUploadImages, for: indexPath) as? UploadImagesCollectionViewCell {
+                cell.fill(material: self.images[indexPath.row])
+                return cell
+            }
+        } else {
+            // Add image
+            return collectionView.dequeueReusableCell(withReuseIdentifier: kCollectionViewCellUploadImages, for: indexPath)
+        }
+        return UICollectionViewCell()
+    }
+    
+    // MARK:- CollectionView Data Source
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.collectionViewImages {
+            return self.images.count + 1
+        }
+        return 0
+    }
 
+    
+    // MARK: CollectionView Delegate
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.row < self.images.count {
+            
+        } else {
+            self.presentActionSheetForImageSourceSelection()
+        }
+    }
+    
     
     // MARK:- Actions
     
@@ -44,6 +106,23 @@ class UploadFilesViewController: UIViewController, UIImagePickerControllerDelega
         self.saveMaterial()
     }
     
+    func presentActionSheetForImageSourceSelection() {
+        let sheet = UIAlertController(title: nil, message: "Selecione a origem da imagem", preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Biblioteca de Fotos", style: .default, handler: { [weak self] (action) in
+            if let imagePicker = self?.imagePicker {
+                imagePicker.sourceType = .photoLibrary
+                self?.present(imagePicker, animated: true, completion: nil)
+            }
+        }))
+        sheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { [weak self] (action) in
+            if let imagePicker = self?.imagePicker {
+                imagePicker.sourceType = .camera
+                self?.present(imagePicker, animated: true, completion: nil)
+            }
+        }))
+        sheet.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        self.present(sheet, animated: true, completion: nil)
+    }
     
     private func saveMaterial() {
         
@@ -72,21 +151,22 @@ class UploadFilesViewController: UIViewController, UIImagePickerControllerDelega
     //MARK:- UIImagePickerController Delegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage,
-            let data = UIImageJPEGRepresentation(image, 0.0) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             let material = Material(kind: .image)
-            material.data = data
-            material.upload(progressHandler: { (fraction) in
-            }, completionHandler: { (error) in
-                
-                if let error = error {
-                    print("Error: \(error.localizedDescription)")
-                    return
-                }
-                
-                print("Material uploaded: \(material.path)")
-                
-            })
+            material.image = image
+            self.images.append(material)
+            self.collectionViewImages.reloadData()
+//            material.upload(progressHandler: { (fraction) in
+//            }, completionHandler: { (error) in
+//                
+//                if let error = error {
+//                    print("Error: \(error.localizedDescription)")
+//                    return
+//                }
+//                
+//                print("Material uploaded: \(material.path)")
+//                
+//            })
         }
         self.imagePicker.dismiss(animated: true, completion: nil)
     }
