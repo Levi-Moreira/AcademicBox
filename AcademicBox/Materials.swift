90d856ffc3de3e7aa1e3bda9cf6cd3aae34e885f
Alan Jeferson <alan.jeferson11@gmail.com>
2017-07-02 18:54:16 -0300

    Improvements on the upload of images

---
+++ AcademicBox/Materials.swift
@@ -0,0 +1,89 @@
+//
+//  Materials.swift
+//  AcademicBox
+//
+//  Created by Alan Jeferson on 02/07/17.
+//  Copyright © 2017 IFCE. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class Materials: NSObject {
+    
+    private let kMaterialsPath = "materials"
+    
+    var name = ""
+    var images = [Material]()
+    var uploadedImages = 0
+    var uploadedImagesError = 0
+    
+    var reference: DatabaseReference {
+        return Database.database().reference(withPath: "materials")
+    }
+    
+    func add(image: Material) {
+        self.images.append(image)
+    }
+    
+    func upload(completionBlock: @escaping (_ error: Error?) -> Void) {
+        
+        self.reference.childByAutoId().setValue(self.toJson()) { [weak self] (error, reference) in
+            
+            if let _ = error {
+                completionBlock(error)
+                return
+            }
+            
+            print("Materials \(reference.key) saved")
+            if let images = self?.images {
+                images.forEach { $0.baseName = reference.key }
+            }
+            
+            completionBlock(nil)
+            
+        }
+        
+    }
+    
+    func uploadImages() {
+        self.images.forEach {
+            $0.upload(progressHandler: { (fraction) in
+            }, completionHandler: { [weak self] (error) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    if let c = self?.uploadedImagesError {
+                        self?.uploadedImagesError = c + 1
+                    }
+                }
+                guard let c = self?.uploadedImages else {
+                    return
+                }
+                self?.uploadedImages = c - 1
+                if(c - 1 <= 0) {
+                    self?.didFinishUploadingImages()
+                }
+            })
+        }
+    }
+    
+    func uploadAudios() {
+    }
+    
+    func uploadVideos() {
+    }
+    
+    func didFinishUploadingImages() {
+    }
+    
+    //    init(json: JSON) {
+    //        self.name = json["name"].stringValue
+    //    }
+    //
+    func toJson() -> [String: Any] {
+        return [
+            "name": self.name
+        ]
+    }
+    
+}
